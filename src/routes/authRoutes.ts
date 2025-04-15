aa286465f5c7cf6d2ae9dd593138010ebe16625a
mvhmxud <mvhmxud@gmail.com>
2025-04-15 05:49:55 +0200

    refactor: :white_check_mark: some auth tweeks
    - Add CORS middleware with credentials support  - Update token generation with user data - Add environment variables for JWT secrets

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,20 @@
 import express from 'express'
 import { signup, login, googleAuth, refreshTokenHandler } from '../controllers/authController'
 const router = express.Router()
+import { body } from 'express-validator'
 
 // Define route handlers with explicit types
-router.post('/signup', signup)
-router.post('/login', login)
+router.post('/signup' , [
+  body('email').isEmail().withMessage('Invalid email address'),
+  body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+  body('username').notEmpty().withMessage('Username is required'),
+  
+], signup)
+router.post('/login' , [
+  body('email').isEmail().withMessage('Invalid email address'),
+  body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+], login)
 router.post('/google', googleAuth)
-router.post("/refresh-token", refreshTokenHandler);
+router.post("/refresh-token" , refreshTokenHandler);
 
 export default router
