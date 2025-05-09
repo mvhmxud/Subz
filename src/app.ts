57e2e260b8fadc2e85497300cf8658d8e129bc62
mvhmxud <mvhmxud@gmail.com>
2025-05-09 11:59:57 +0300

    test: :heavy_plus_sign: Add  CORS  headers

--- src/app.ts
+++ src/app.ts
@@ -34,6 +34,17 @@ app.use(
   })
 );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  ); 
+  next();
+});
+
 app.use("/api/users", AuthMiddleware, userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/chats", AuthMiddleware, chatRoutes);
